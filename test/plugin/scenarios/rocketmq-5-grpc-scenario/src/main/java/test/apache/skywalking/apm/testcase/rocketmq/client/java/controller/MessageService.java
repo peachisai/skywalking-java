a4994a59681cda3c5fe6c13dfb6b5c9339a9a255
peachisai <2581009893@qq.com>
2026-02-21 23:30:04 +0800

    Revert "Begin 9.7.0 iteration, 9.6.0 is tagged for release. (#791)"
    This reverts commit b03d63c8babcface3498344bf5feb1be2883393d.

--- test/plugin/scenarios/rocketmq-5-grpc-scenario/src/main/java/test/apache/skywalking/apm/testcase/rocketmq/client/java/controller/MessageService.java
+++ test/plugin/scenarios/rocketmq-5-grpc-scenario/src/main/java/test/apache/skywalking/apm/testcase/rocketmq/client/java/controller/MessageService.java
@@ -203,23 +203,26 @@ public class MessageService {
                 maxMessageNum,
                 invisibleDuration
             );
-            future0.thenComposeAsync(messages -> {
+            future0.whenCompleteAsync((messages, throwable) -> {
+                if (null != throwable) {
+                    log.error("Failed to receive message from remote", throwable);
+                    return;
+                }
                 log.info("Received {} message(s)", messages.size());
                 final Map<MessageView, CompletableFuture<Void>> map =
                     messages.stream().collect(Collectors.toMap(message -> message, consumer::ackAsync));
-                List<CompletableFuture<Void>> ackFutures = map.entrySet().stream().map(entry -> {
+                for (Map.Entry<MessageView, CompletableFuture<Void>> entry : map.entrySet()) {
                     final MessageId messageId = entry.getKey().getMessageId();
                     final CompletableFuture<Void> future = entry.getValue();
-                    return future.whenCompleteAsync((v, t) -> {
+                    future.whenCompleteAsync((v, t) -> {
                         if (null != t) {
                             log.error("Message is failed to be acknowledged, messageId={}", messageId, t);
                             return;
                         }
                         log.info("Message is acknowledged successfully, messageId={}", messageId);
                     }, ackCallbackExecutor);
-                }).collect(Collectors.toList());
-                return CompletableFuture.allOf(ackFutures.toArray(new CompletableFuture[0]));
-            }, receiveCallbackExecutor).join();
+                }
+            }, receiveCallbackExecutor);
         } catch (Exception e) {
             log.error("consumer start error", e);
         }
@@ -240,8 +243,6 @@ public class MessageService {
         MQAdminStartup.main(subArgs);
     }
 
-    public static volatile boolean CONSUMER_READY = false;
-
     public static class MyConsumer implements MessageListener {
 
         @Override
@@ -249,7 +250,6 @@ public class MessageService {
             log.info("Consume message successfully, messageId={},messageBody={}", messageView.getMessageId(),
                      messageView.getBody().toString()
             );
-            CONSUMER_READY = true;
             return ConsumeResult.SUCCESS;
         }
     }
